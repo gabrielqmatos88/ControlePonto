ba3e0cab89cf7028cf17e1dfb0a5718213c2cd83
Gabriel Q. Matos <gabriel.q.matos@gmail.com>
2013-12-12 01:53:48 -0200

    Criação da Classe UsuarioDao para consumir o usuario do banco

---
+++ Controle.Ponto.Library/Entidades/DAO/UsuarioDao.cs
@@ -0,0 +1,77 @@
+﻿using Controle.Ponto.Library.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle.Ponto.Library.Entidades.DAO
+{
+    public static class UsuarioDao
+    {
+        /// <summary>
+        /// Consultar um usuário por Id
+        /// </summary>
+        /// <param name="contexto">contexto do banco</param>
+        /// <param name="id">id do usuario</param>
+        /// <returns></returns>
+        public static Usuario ConsultarUsuario(PontoContext contexto, int id) 
+        {
+            return contexto.Usuarios.Find(id);
+        }
+
+        /// <summary>
+        /// Consultar um usuário por matricula
+        /// </summary>
+        /// <param name="contexto">contexto do banco</param>
+        /// <param name="matricula">matricula do usuario</param>
+        /// <returns></returns>
+        public static Usuario ConsultarUsuarioPorMatricula(PontoContext contexto, int matricula) 
+        {
+            return contexto.Usuarios.SingleOrDefault(o=>o.Matricula.Equals(matricula));
+        }
+
+        /// <summary>
+        /// Salvar um usuario no contexto
+        /// </summary>
+        /// <param name="contexto"></param>
+        /// <param name="usuario"></param>
+        /// <returns></returns>
+        public static bool SalvarUsuario(PontoContext contexto, Usuario usuario) 
+        {
+            Usuario usuarioCadastrado = ConsultarUsuario(contexto,usuario.Id);
+            if (usuarioCadastrado != null) 
+                contexto.Entry(usuario).State = EntityState.Modified;
+            else
+                contexto.Usuarios.Add(usuarioCadastrado);
+            return contexto.SaveChanges() == 1;
+        }
+
+        /// <summary>
+        /// Excluir um usuario (Exclusão lógica)
+        /// </summary>
+        /// <param name="contexto">contexto do banco</param>
+        /// <param name="usuario">usuario</param>
+        public static void ExcluirUsuario(PontoContext contexto, Usuario usuario) 
+        {
+            ExcluirUsuario(contexto, usuario.Id);
+        }
+
+        /// <summary>
+        /// Excluir um usuario (Exclusão lógica)
+        /// </summary>
+        /// <param name="contexto">contexto do banco</param>
+        /// <param name="id">id do usuario</param>
+        public static void ExcluirUsuario(PontoContext contexto,int id) 
+        {
+            Usuario usuario = ConsultarUsuario(contexto, id);
+
+            if (usuario == null || usuario.CsExcluido)
+                return;
+
+            usuario.CsExcluido = true;
+            contexto.SaveChanges();
+        }
+    }
+}
