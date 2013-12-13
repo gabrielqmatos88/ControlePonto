1ca5c639de444e1ff5a5a93e8ddac9f547750426
Gabriel Q. Matos <gabriel.q.matos@gmail.com>
2013-12-12 20:03:57 -0400

    Criação do projeto de testes unitários do Controle de Ponto

---
+++ Controle.Ponto.Library/Libs/GestorPonto.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Controle.Ponto.Library.Modelo.Contexto;
+using Controle.Ponto.Library.Modelo.Entidades;
+
+namespace Controle.Ponto.Library.Libs
+{
+    public class GestorPonto
+    {
+        private List<DiaPonto> diasPonto;
+
+        /// <summary>
+        /// Dias de registro de ponto
+        /// </summary>
+        public List<DiaPonto> DiasPonto
+        {
+            get { return diasPonto; }
+            set { diasPonto = value; }
+        }
+
+        private List<Registro> registrosPonto;
+
+        /// <summary>
+        /// Registros de ponto
+        /// </summary>
+        public List<Registro> RegistrosPonto
+        {
+            get { return registrosPonto; }
+            set { registrosPonto = value; }
+        }
+
+        private Usuario usuarioPonto;
+
+        /// <summary>
+        /// Usuario do ponto
+        /// </summary>
+        public Usuario UsuarioPonto
+        {
+            get { return usuarioPonto; }
+            set { usuarioPonto = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dias"></param>
+        /// <param name="registros"></param>
+        /// <param name="usuario"></param>
+        public GestorPonto(List<DiaPonto> dias, List<Registro> registros, Usuario usuario)
+        {
+            if(dias == null || registros == null || usuario == null)
+                throw new ArgumentNullException( "Devem ser preenchidos todos os argumentos para o gestor de ponto." );
+
+            diasPonto = dias;
+            registrosPonto = registros;
+            usuarioPonto = usuario;
+        }
+
+        public List<Registro> ListarRegistrosDataSelecionada(DateTime data)
+        {
+            return null;
+        }
+
+        public DiaPonto SelecionarDiaPontoPorData(DateTime data)
+        {
+            return null;
+        }
+
+        public TimeSpan CalcularHorasTrabalhadas() 
+        {
+            return TimeSpan.Zero;
+        }
+    }
+}
---
+++ Controle.Ponto.Testes/GestorPontoTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using Controle.Ponto.Library.Libs;
+using Controle.Ponto.Library.Modelo.Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Controle.Ponto.Testes
+{
+    [TestClass]
+    public class GestorPontoTest
+    {
+        private GestorPonto gestor;
+
+        private List<DiaPonto> dias;
+
+        private List<Registro> registros;
+
+        private List<Usuario> usuarios;
+
+        [TestInitialize]
+        public void Inicializador()
+        {
+            InicializarUsuarios();
+            InicializarRegistros();
+            InicializarDias();
+        }
+
+        public void InicializarDias()
+        {
+            dias = new List<DiaPonto>();
+            dias.Add( new DiaPonto() { Id = 1, Data = new DateTime( 2013, 12, 7 ),  IdUsuario = 1 } );
+            dias.Add( new DiaPonto() { Id = 2, Data = new DateTime( 2013, 12, 4 ),  IdUsuario = 2 } );
+            dias.Add( new DiaPonto() { Id = 3, Data = new DateTime( 2013, 12, 2 ),  IdUsuario = 3 } );
+            dias.Add( new DiaPonto() { Id = 4, Data = new DateTime( 2013, 12, 3 ),  IdUsuario = 3 } );
+            dias.Add( new DiaPonto() { Id = 5, Data = new DateTime( 2013, 12, 6 ),  IdUsuario = 2 } );
+            dias.Add( new DiaPonto() { Id = 6, Data = new DateTime( 2013, 12, 2 ),  IdUsuario = 1 } );
+            dias.Add( new DiaPonto() { Id = 7, Data = new DateTime( 2013, 12, 6 ),  IdUsuario = 1 } );
+            dias.Add( new DiaPonto() { Id = 8, Data = new DateTime( 2013, 12, 5 ),  IdUsuario = 1 } );
+            dias.Add( new DiaPonto() { Id = 9, Data = new DateTime( 2013, 12, 3 ),  IdUsuario = 2 } );
+            dias.Add( new DiaPonto() { Id = 10, Data = new DateTime( 2013, 12, 2 ), IdUsuario = 3 } );
+            dias.Add( new DiaPonto() { Id = 11, Data = new DateTime( 2013, 12, 7 ), IdUsuario = 2 } );
+            dias.Add( new DiaPonto() { Id = 12, Data = new DateTime( 2013, 12, 7 ), IdUsuario = 3 } );
+        }
+
+        public void InicializarUsuarios()
+        {
+            usuarios = new List<Usuario>();
+            usuarios.Add( new Usuario() { Id = 1, Matricula = 001, Nome = "Joao Neto" , Senha = "123" } );
+            usuarios.Add( new Usuario() { Id = 3, Matricula = 002, Nome = "Marcio Nery", Senha = "123" } );
+            usuarios.Add( new Usuario() { Id = 2, Matricula = 003, Nome = "Reinaldo Thompson", Senha = "123" } );
+        }
+
+        public void InicializarRegistros()
+        {
+            registros = new List<Registro>();
+            registros.Add( new Registro() { Id = 1, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 2, IdDiaPonto = 1, Hora = new TimeSpan( 12, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 3, IdDiaPonto = 1, Hora = new TimeSpan( 13, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 4, IdDiaPonto = 1, Hora = new TimeSpan( 17, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 5, IdDiaPonto = 1, Hora = new TimeSpan( 9, 40, 0 ) } );
+            registros.Add( new Registro() { Id = 6, IdDiaPonto = 1, Hora = new TimeSpan( 12, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 7, IdDiaPonto = 1, Hora = new TimeSpan( 17, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 8, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 9, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 10, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 11, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 12, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 13, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 14, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 15, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 16, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 17, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 18, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 19, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 20, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 21, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 22, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 23, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 24, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 25, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 26, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 27, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 28, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+            registros.Add( new Registro() { Id = 29, IdDiaPonto = 1, Hora = new TimeSpan( 8, 0, 0 ) } );
+        }
+
+        [TestMethod]
+        public void DeveSelecionarODiaDePontoParaADataSelecionada()
+        {
+
+        }
+
+        [TestMethod]
+        public void DeveSelecionarOsRegistrosParaADataSelecionada()
+        {
+
+        }
+
+    }
+}
